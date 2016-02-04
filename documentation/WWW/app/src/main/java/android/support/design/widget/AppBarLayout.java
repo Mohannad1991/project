806311c8615f4aacc642c3f41a95f2367644c5f3
mohannad1991 <ma3r5@mail.umkc.edu>
2016-02-03 22:52:34 -0600

    homework 1

---
+++ documentation/WWW/app/src/main/java/android/support/design/widget/AppBarLayout.java
@@ -0,0 +1,14 @@
+package android.support.design.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2/3/2016.
+ */
+public class AppBarLayout extends View {
+    public AppBarLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
